5b4500600965bff705318823168a2dde409ba102
Michael Shick <michael.shick@gmail.com>
2015-03-21 16:25:49 -0400

    fix bin name

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='CoverLovin',
-    version='2.0.3',
+    version='2.0.4',
     author='James Stewart',
     author_email='webmaster@jimter.net',
     packages=['coverlovin'],
-    scripts=['bin/coverlovin.py'],
+    scripts=['bin/coverlovin'],
     url='https://launchpad.net/coverlovin/',
     description='Recursively parse audio files and download cover art.',
     long_description='Recursively process subdirectories of given directory, downloading appropriate cover images from Google Images if .mp3 files are found.',
