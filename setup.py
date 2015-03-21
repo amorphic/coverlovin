d8fddba66f74f55195ad73d77606b7796c5fa316
Michael Shick <michael.shick@gmail.com>
2015-03-21 16:16:22 -0400

    version bump for release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='CoverLovin',
-    version='2.0.1',
+    version='2.0.2',
     author='James Stewart',
     author_email='webmaster@jimter.net',
     packages=['coverlovin'],
